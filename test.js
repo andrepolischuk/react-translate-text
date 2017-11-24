a5b4f3c4a15f9e7fdf5af0e447235396ea9608c8
Andrey Polischuk <me@andrepolischuk.com>
2017-11-24 14:19:18 +0300

    Bump translate-text

--- test.js
+++ test.js
@@ -3,15 +3,9 @@ import React from 'react'
 import pluralize from 'numd'
 import {mount} from 'enzyme'
 import {TranslateProvider, withTranslate, createTranslate} from './src'
+import translation from './fixture'
 
-const translation = {
-  foo: 'foofoo',
-  bar: {
-    foo: 'barbar',
-    baz: 'bazbaz $1',
-    foobar: 'foobar foo $1 (pluralize $2|foo|foos)'
-  }
-}
+const echo = v => v
 
 const App = withTranslate(({translate}) => (
   <div>
@@ -19,12 +13,13 @@ const App = withTranslate(({translate}) => (
     <p>{translate('bar.foo')}</p>
     <p>{translate('bar.baz', 'foo')}</p>
     <p>{translate('bar.foobar', 'bar', 2)}</p>
+    <p>{translate('bar.foobarbaz', 'foo')}</p>
   </div>
 ))
 
 test('should pass translation object', t => {
   const wrapper = mount(
-    <TranslateProvider translation={translation} helpers={{pluralize}}>
+    <TranslateProvider translation={translation} helpers={{echo, pluralize}}>
       <App />
     </TranslateProvider>
   )
@@ -33,10 +28,11 @@ test('should pass translation object', t => {
   t.is(wrapper.find('p').at(1).text(), 'barbar')
   t.is(wrapper.find('p').at(2).text(), 'bazbaz foo')
   t.is(wrapper.find('p').at(3).text(), 'foobar foo bar 2 foos')
+  t.is(wrapper.find('p').at(4).text(), 'foobarbaz foo')
 })
 
 test('should pass traslate function', t => {
-  const translate = createTranslate(translation, {pluralize})
+  const translate = createTranslate(translation, {echo, pluralize})
 
   const wrapper = mount(
     <TranslateProvider translate={translate}>
@@ -48,4 +44,5 @@ test('should pass traslate function', t => {
   t.is(wrapper.find('p').at(1).text(), 'barbar')
   t.is(wrapper.find('p').at(2).text(), 'bazbaz foo')
   t.is(wrapper.find('p').at(3).text(), 'foobar foo bar 2 foos')
+  t.is(wrapper.find('p').at(4).text(), 'foobarbaz foo')
 })
