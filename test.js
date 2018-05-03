f10e7f36b6584a5e9f15a583f535e4b770c832c3
Andrey Polischuk <me@andrepolischuk.com>
2018-05-03 16:01:53 +0300

    Support arrays in translation

--- test.js
+++ test.js
@@ -1,48 +1,38 @@
 import test from 'ava'
 import React from 'react'
-import pluralize from 'numd'
 import {mount} from 'enzyme'
 import {TranslateProvider, withTranslate, createTranslate} from './src'
-import translation from './fixture'
 
 const echo = v => v
 
-const App = withTranslate(({translate}) => (
+const translation = {
+  hello: 'Hello (echo $1)!'
+}
+
+const App = withTranslate(({name, translate}) => (
   <div>
-    <p>{translate('foo')}</p>
-    <p>{translate('bar.foo')}</p>
-    <p>{translate('bar.baz', 'foo')}</p>
-    <p>{translate('bar.foobar', 'bar', 2)}</p>
-    <p>{translate('bar.foobarbaz', 'foo')}</p>
+    {translate('hello', name)}
   </div>
 ))
 
 test('should pass translation object', t => {
   const wrapper = mount(
-    <TranslateProvider translation={translation} helpers={{echo, pluralize}}>
-      <App />
+    <TranslateProvider translation={translation} helpers={{echo}}>
+      <App name='World' />
     </TranslateProvider>
   )
 
-  t.is(wrapper.find('p').at(0).text(), 'foofoo')
-  t.is(wrapper.find('p').at(1).text(), 'barbar')
-  t.is(wrapper.find('p').at(2).text(), 'bazbaz foo')
-  t.is(wrapper.find('p').at(3).text(), 'foobar foo bar 2 foos')
-  t.is(wrapper.find('p').at(4).text(), 'foobarbaz foo')
+  t.is(wrapper.text(), 'Hello World!')
 })
 
 test('should pass traslate function', t => {
-  const translate = createTranslate(translation, {echo, pluralize})
+  const translate = createTranslate(translation, {echo})
 
   const wrapper = mount(
     <TranslateProvider translate={translate}>
-      <App />
+      <App name='World' />
     </TranslateProvider>
   )
 
-  t.is(wrapper.find('p').at(0).text(), 'foofoo')
-  t.is(wrapper.find('p').at(1).text(), 'barbar')
-  t.is(wrapper.find('p').at(2).text(), 'bazbaz foo')
-  t.is(wrapper.find('p').at(3).text(), 'foobar foo bar 2 foos')
-  t.is(wrapper.find('p').at(4).text(), 'foobarbaz foo')
+  t.is(wrapper.text(), 'Hello World!')
 })
